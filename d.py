ebf8ee5bcc4bc6fc5834ba4af0c33b0f586e04c7
rani420 <60012268+rani420@users.noreply.github.com>
2020-01-17 23:00:48 +0530

    Add files via upload

---
+++ d.py
@@ -0,0 +1,29 @@
+
+import requests as req
+from bs4 import BeautifulSoup as bs
+for re in range(1,3):
+    if re ==1:
+        url = 'http://masalaseen.com'
+    else:
+        url = f'http://masalaseen.com/page/{re}'
+    print(url)
+    r1 = req.get(url).content
+    s1 = bs(r1, 'html.parser')
+    a = s1.find_all('a', title=True, rel=False)
+    a.pop()
+    a.pop()
+    ah = []
+    for i in a:
+        ah.append(i['href'])
+    vs = []
+    for j in ah:
+        r2 = req.get(j).content
+        s2 = bs(r2, 'html.parser')
+        vid = s2.find('source')['src']
+        img = s2.find_all('img')[2]['src']
+        payload = {
+            'num1':img,
+            'num2':vid
+        }
+        r3 = req.get('https://for.pythonanywhere.com/upload', params=payload)
+        print(r3.content)
