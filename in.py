46a3c78e8cce8c77e6c5637076cc6f67f5af9416
rani420 <60012268+rani420@users.noreply.github.com>
2020-02-02 17:37:31 +0530

    Update in.py

--- in.py
+++ in.py
@@ -7,8 +7,8 @@
 
 q_hash = 'e769aa130647d2354c40ea6a439bfc08'
 iid = '5856087167'
-after = 'QVFDaFNEbWQ1ZWRKeDN2UWtSWmdBejV4NW13Q3RoWnI0aGYxVmdmOHRmQm50S0VjYWRWdUJjcXlvbjFJWWlqQkNKTDNyXzc1VTc3anlQdkRGb19VdnVfdg%3D%3D'
-count = 12
+after = ''
+count = 50
 
 api_endpoint = f'https://www.instagram.com/graphql/query/?query_hash={q_hash}&variables=%7B%22id%22%3A%22{iid}%22%2C%22first%22%3A{count}%2C%22after%22%3A%22{after}%22%7D'
 
@@ -18,9 +18,24 @@ from bs4 import BeautifulSoup as bs
 r = req.get(api_endpoint)
 r_dict = r.json()
 type(r_dict)
-img = r_dict['data']['user']['edge_owner_to_timeline_media']['edges'][0]['node']['display_url']
+c = int(input('enter key'))
+img = r_dict['data']['user']['edge_owner_to_timeline_media']['edges'][c]['node']['display_url']
 end_cursor = r_dict['data']['user']['edge_owner_to_timeline_media']['page_info']['end_cursor']
-shortcode = r_dict['data']['user']['edge_owner_to_timeline_media']['edges'][0]['node']['shortcode']
+shortcode = r_dict['data']['user']['edge_owner_to_timeline_media']['edges'][c]['node']['shortcode']
 print(img)
 print(end_cursor)
 print(shortcode)
+u = f'https://instagram.com/p/{shortcode}'
+r1 = req.get(u).content
+s1 = bs(r1, 'html.parser')
+print(s1.title.text)
+m = s1.find_all('script')[3].text[21:-1]
+import json
+j = json.loads(m)
+d = json.dumps(j, indent=4)
+# v1 = j['entry_data']['PostPage'][0]['graphql']['shortcode_media']['edge_sidecar_to_children']['edges'][4]['node']['video_url']
+le = len(j['entry_data']['PostPage'][0]['graphql']['shortcode_media']['edge_sidecar_to_children']['edges'])
+for h in range(le):
+    v = j['entry_data']['PostPage'][0]['graphql']['shortcode_media']['edge_sidecar_to_children']['edges'][h]['node']['video_url']
+    print(v)
+    print('\n')
